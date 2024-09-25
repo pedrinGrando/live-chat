d0c16fbd23ea2f9fc1529ed59102a03a00d7a6da
Pedro Aluisio Scuissiatto <pedrogrando6@gmail.com>
2024-09-25 14:39:52 -0300

    add websocket config

---
+++ live-chat-ms/src/main/java/com/pedro/livechatms/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.pedro.livechatms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+       registry.enableSimpleBroker("/topics");
+       registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       registry.addEndpoint("/pedro-livechat-websocket");
+    }
+}
---
+++ live-chat-ms/src/test/java/com/pedro/livechatms/LiveChatMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.livechatms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
